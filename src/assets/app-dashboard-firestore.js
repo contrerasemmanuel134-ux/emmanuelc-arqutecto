52ba78bbc050f9e15d901e5ffeb78a3ffc523dcb
contrerasemmanuel134-ux <contrerasemmanuel134@gmail.com>
2025-09-02 23:46:25 +0000

    debug
    Signed-off-by: contrerasemmanuel134-ux <contrerasemmanuel134@gmail.com>

--- src/assets/app-dashboard-firestore.js
+++ src/assets/app-dashboard-firestore.js
@@ -1,3 +1,8 @@
+// app-dashboard-firestore.js
+
+// **1. Añade esta línea JUSTO al principio del archivo (línea 1)**
+console.log("--- DEBUG: app-dashboard-firestore.js INICIADO ---");
+
 import { getFirestore, collection, getDocs, orderBy, query, addDoc, serverTimestamp, doc, getDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 document.addEventListener('DOMContentLoaded', () => {
