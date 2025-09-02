bb098b324f5400094defdc3a873473a5e01b9b71
contrerasemmanuel134-ux <contrerasemmanuel134@gmail.com>
2025-09-02 18:12:21 +0000

    solucion al problema autenticacion
    Signed-off-by: contrerasemmanuel134-ux <contrerasemmanuel134@gmail.com>

--- public/firebase/client.js
+++ public/firebase/client.js
@@ -1,4 +1,4 @@
-// src/firebase/client.js
+// public/firebase/client.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFunctions } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-functions.js";
